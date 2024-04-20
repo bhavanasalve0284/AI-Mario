04ca8c3dd600ccfdc0c0aed1ad06b456064c539d
bhavanasalve0284 <112340797+bhavanasalve0284@users.noreply.github.com>
2024-04-20 14:29:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_die = loadSound("mariodie.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas=createCanvas(1240,336);
+	instializeInSetup(mario);
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
